339f6689d2cd6a257b7638a1e0cc5de629b905b3
Keido Sumi <delta0726@nta.pial.jp>
2021-02-04 22:39:15 +0900

    Update usage of coBC

--- library/SSLR/function/classification/coBC.R
+++ library/SSLR/function/classification/coBC.R
@@ -8,19 +8,26 @@
 
 
 # ＜概要＞
-# -
+# - CoBCは共同トレーニングスタイルの半教師あり学習アルゴリズム
+#   --- ラベル付けされたサンプルの削減されたセットを使用して、学習器の引数で定義された学習スキームでN個の分類器をトレーニング
+#   --- 最終的な予測は、N個のリグレッサーの推定値の平均
 
 
 # ＜構文＞
 # coBC(learner, N = 3, perc.full = 0.7, u = 100, max.iter = 50)
+#
+# - learner   ： Parsnipで作成した分類モードの学習器
+# - N         ： コミッティーのメンバーとして
+# - perc.full ： 新しいラベル付きの例の割合がこの値に達すると自己ラベル付けプロセスが停止(0-1)
+# - u         ： プール内のラベルのないインスタンスの数
+# - max.iter  ： 自己ラベル付けプロセスで実行する反復の最大数
 
 
 # ＜目次＞
 # 0 準備
 # 1 データ作成
-# 2 ベースモデル
-# 3 共学習
-# 4 スキーマありの共学習
+# 2 ラベル欠損の作成
+# 3 モデリング
 
 
 # 0 準備 -------------------------------------------------------------------------------
@@ -39,32 +46,61 @@ data(wine)
 
 # データ分割
 set.seed(1)
-train.index <- createDataPartition(wine$Wine, p = .7, list = FALSE)
+train.index <- wine$Wine %>% createDataPartition(p = .7, list = FALSE)
 train <- wine[ train.index,]
 test  <- wine[-train.index,]
 
+# 行列数
+train %>% dim()
+test %>% dim()
+
+# データ確認
+train %>% glimpse()
+
 # 列番号の取得
+# --- Wineは14列目
 cls <- which(colnames(wine) == "Wine")
 
-# ラベルをNAに置換
-labeled.index <- createDataPartition(wine$Wine, p = .2, list = FALSE)
-train[-labeled.index,cls] <- NA
+
+# 2 ラベル欠損の作成 ---------------------------------------------------------------------
+
+# ラベルにNAを挿入
+labeled.index <- wine$Wine %>% createDataPartition(p = 0.2, list = FALSE)
+train[-labeled.index, cls] <- NA
+
+# Wine列の確認
+train$Wine %>% print()
+train$Wine %>% summary()
+
+# 確認
+train %>% print()
 
 
-# 2 モデリング ----------------------------------------------------------------------------
+# 3 モデリング ----------------------------------------------------------------------------
 
 # ベースモデル
+# --- Parsnipで作成
+# --- 未学習（フォーミュラ定義もしていない）
 rf <-
   rand_forest(trees = 100, mode = "classification") %>%
   set_engine("randomForest")
 
 # 共学習モデル
+# --- 訓練データで学習
+# --- 大半のラベルがNA
 m <-
-  coBC(learner = rf, N = 3, erc.full = 0.7, u = 100, max.iter = 3) %>%
-    fit(Wine ~ ., data = train)
+  coBC(learner = rf, N = 3, perc.full = 0.7, u = 100, max.iter = 3) %>%
+     fit(Wine ~ ., data = train)
+
+
+# 4 モデル評価 ---------------------------------------------------------------------------
+
+# 予測
+pred <-
+  m %>%
+    predict(test) %>%
+    bind_cols(test) %>%
+    select(Wine, .pred_class)
 
 # モデル精度の検証
-m %>%
-  predict(test) %>%
-  bind_cols(test) %>%
-  metrics(truth = "Wine", estimate = .pred_class)
+pred %>% metrics(truth = "Wine", estimate = .pred_class)
